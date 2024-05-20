994612663b9b33a9bb1fc75ca9317bfc27868946
Hippozhibos <zhiboz1012@163.com>
2024-05-20 12:21:41 +0800

    regular update

--- assets/CyberMice_test.py
+++ assets/CyberMice_test.py
@@ -38,16 +38,16 @@ def walker_run(random_state=None):
 
 viewer.launch(environment_loader=walker_run)
 
-# import numpy as np
-# env = walker_run()
+import numpy as np
+env = walker_run()
 
-# # Get the `action_spec` describing the control inputs.
-# action_spec = env.action_spec()
+# Get the `action_spec` describing the control inputs.
+action_spec = env.action_spec()
 
-# # Step through the environment for one episode with random actions.
-# time_step = env.reset()
-# while not time_step.last():
-#     action = np.random.uniform(action_spec.minimum, action_spec.maximum, size=action_spec.shape)
-#     time_step = env.step(action)
-#     print("reward = {}, discount = {}, observations = {}.".format(
-#     time_step.reward, time_step.discount, time_step.observation))
+# Step through the environment for one episode with random actions.
+time_step = env.reset()
+while not time_step.last():
+    action = np.random.uniform(action_spec.minimum, action_spec.maximum, size=action_spec.shape)
+    time_step = env.step(action)
+    print("reward = {}, discount = {}, observations = {}.".format(
+    time_step.reward, time_step.discount, time_step.observation))
